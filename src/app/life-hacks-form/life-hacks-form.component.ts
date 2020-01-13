66f0955983a28d753fbd00ff7bf0325b6e6eab2f
JadyVella <felixouma379@gmail.com>
2020-01-13 10:55:04 +0300

    - create a form component - import FormsModule to the app.module.ts - Add name, description, and date imputs in life-hacks-form.component.html - Import LifeHacks class to the life-hacks-component.ts

--- src/app/life-hacks-form/life-hacks-form.component.ts
+++ src/app/life-hacks-form/life-hacks-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LifeHacks } from '../life-hacks'
 
 @Component({
   selector: 'app-life-hacks-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LifeHacksFormComponent implements OnInit {
 
+  newLifeHacks = new LifeHacks(0,"","","");
   constructor() { }
 
   ngOnInit() {
