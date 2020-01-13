2abc481990824a5fd02ab482d9904eefcc79e3b3
JadyVella <felixouma379@gmail.com>
2020-01-13 11:18:01 +0300

    - Create a pipe called dateCount - Add date to function to the pipe - Add the date function in the life-hacks-details.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { LifeHacksDetailsComponent } from './life-hacks-details/life-hacks-details.component';
 import { LifeHacksComponent } from './life-hacks/life-hacks.component';
 import { LifeHacksFormComponent } from './life-hacks-form/life-hacks-form.component';
+import { ShowvotesDirective } from './showvotes.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LifeHacksDetailsComponent,
     LifeHacksComponent,
-    LifeHacksFormComponent
+    LifeHacksFormComponent,
+    ShowvotesDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
