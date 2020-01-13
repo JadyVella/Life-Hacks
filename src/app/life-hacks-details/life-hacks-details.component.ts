5f525d6a5b37e1950d2e6b56aa5ca2d9717d90e2
JadyVella <felixouma379@gmail.com>
2020-01-13 16:58:41 +0300

    Add a like and dislike buttons in life-hacks-details.html

--- src/app/life-hacks-details/life-hacks-details.component.ts
+++ src/app/life-hacks-details/life-hacks-details.component.ts
@@ -7,8 +7,8 @@ import { LifeHacks } from "../life-hacks"
   styleUrls: ['./life-hacks-details.component.css']
 })
 export class LifeHacksDetailsComponent implements OnInit {
-  clickCounter: number = 0;
-  dislikeCounter: number = 0;
+  // clickCounter: number = 0;
+  // dislikeCounter: number = 0;
 
   @Input() lifehacks: LifeHacks;
   @Output() toDelete = new EventEmitter<boolean>();
@@ -18,13 +18,31 @@ export class LifeHacksDetailsComponent implements OnInit {
   }
   constructor() { }
 
-  ngOnInit() {
-  }
-  countClick() {
-    this.clickCounter += 1;
-  }
-  countDislike() {
-    this.dislikeCounter += 1;
+  likeQuo = () => {
+    document.getElementById('display').addEventListener('click', () => { like(1); });
+    document.getElementById('dislike').addEventListener('click', () => { like(0); });
+    function like(lk) {
+      var likes, dlikes;
+
+
+      likes = parseInt(document.getElementById('lk').innerText);
+      dlikes = parseInt(document.getElementById('dlk').innerText);
+      if (lk == 0) { dlikes ++; }
+      else { likes ++; }
+      if (likes <= 0) { likes = 0; } if (dlikes <= 0) { dlikes = 0; }
+      document.getElementById('lk').innerText = likes;
+      document.getElementById('dlk').innerText = dlikes;
+    }
   }
 
+  ngOnInit() {
+  }
+  // countClick() {
+  //   this.clickCounter += 1;
+  // }
+  // countDislike() {
+  //   this.dislikeCounter += 1;
+  // }
+
 }
+
